edfc707421091f223278e45f1050df4784c0c91d
Ingrid.Benito <ingrid.benito@hotmail.com>
2022-03-19 19:24:16 +0100

    style css body changes

---
+++ index.js
@@ -0,0 +1,24 @@
+
+/*function hello() {
+    return "Hello, World!";
+};
+
+console.log(hello());
+//Strings
+let cat = 'Vincent';
+const theCake = "a lie";
+const carName = 'Volvo';
+cat = 'Vincent the great';
+
+//Arrays
+const fruits = ['apple', 'strawberries', 'oranges'];
+const schedule = ["eat", "watch tv", "sleep"];
+let animals = ['cat', 'fox', 'dog', 'turtle'];
+
+//Objects
+const car = {
+    type: 'Volvo',
+};
+*/
+
+console.log('ESTO ES UNA PRUEBA');
